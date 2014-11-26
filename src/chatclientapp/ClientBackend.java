a6bed9f91f14039e0c713f89a3548bcd547edd49
Ohjelmistokehitys <Ohjelmistokehitys@OpiFrame2-THINK>
2014-11-26 14:53:32 +0200

    commit 1

---
+++ src/chatclientapp/ClientBackend.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatclientapp;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+//import java.io.OutputStream;
+import java.net.Socket;
+import message.ChatMessage;
+
+/**
+ *
+ * @author Ohjelmistokehitys
+ */
+public class ClientBackend implements Runnable {
+
+    private Socket clientSocket;
+    private ObjectOutputStream output;
+    private ObjectInputStream input;
+    //private OutputStream out;
+    
+    public ClientBackend() {
+        try {
+            clientSocket = new Socket("localhost", 3013);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    @Override
+    public void run() {
+        
+        if (clientSocket == null)
+            return;
+        try {
+            // Output stream must be opened first
+            output = new ObjectOutputStream(clientSocket.getOutputStream());
+            input = new ObjectInputStream(clientSocket.getInputStream());
+            //out = clientSocket.getOutputStream();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+        // read and write from socket until user closes the app
+        while(true) {
+            try {
+                ChatMessage cm = (ChatMessage)input.readObject();
+                System.out.println(cm.getChatMessage());
+            } catch (IOException | ClassNotFoundException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    
+    public void sendMessage(ChatMessage cm) {
+        try {
+            output.writeObject(cm);
+            output.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+}
